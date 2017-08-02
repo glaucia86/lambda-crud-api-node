12d3a9224c1b189d5a797b0b2a564fa1ac2682a1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-01 22:36:54 -0300

    Início do desenvolvimento da classe modelo no arquivo 'post.js'

---
+++ src/GL.API/models/post.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Arquivo: GL.API/models/post.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelo modelo: 'Post' para realizar a conexão com a base
+ *  de dados via Moongose.
+ * Data: 01/08/2017
+ * 
+ */
+
+/**
+ * Infos inerentes a classe: 
+ * ==== Post ====
+ * id: number
+ * title: string
+ * name: string
+ * email: string
+ * body: string
+ */
+
