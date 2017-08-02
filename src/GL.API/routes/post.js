3f6964b090663b2d6f3f270472f1dc4424cf2cb6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-01 22:58:20 -0300

    Desenvolvimento da rota: 'getAllPosts'

--- src/GL.API/routes/post.js
+++ src/GL.API/routes/post.js
@@ -10,3 +10,17 @@
 //Importando o pacote 'mongoose' e o esquema criado na pasta 'models':
 var mongoose = require('mongoose');
 var Post = require('../models/post');
+
+//==> 1) Método: 'getAllPosts' (acessar em: GET: http://localhost:8000/post)
+function getAllPosts(req, res) {
+    
+    var query = Post.find({});
+    query.exec(function(error, posts) {
+        if(error)
+            //res.status(400).send('Erro ao Selecionar todos os Posts!'); //testar..
+            //res.status(400).json(error, 'Erro ao Selecionar todos os Posts!');
+            res.send(error);
+        //res.status(200).json(posts); //testar depois....
+        res.json(posts);
+    });
+}
