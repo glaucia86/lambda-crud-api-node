13b1dab22eeacfda6c232ead00687ec00aa59d9f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-02 10:15:10 -0300

    Definição dos pacotes necessários para a realização do TDD

--- src/test/post.js
+++ src/test/post.js
@@ -6,3 +6,14 @@
  * Data: 21/10/2016
  *
  */
+
+process.env.NODE_ENV = 'test';
+
+var mongoose = require('mongoose');
+var Post = require('../api/models/post');
+
+//Pacotes para realização dos testes no Node.Js: Mocha & Chai
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
