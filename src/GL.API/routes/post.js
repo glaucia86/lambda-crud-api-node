28e54cf8fe8120a6703fe478e36d001c272ac616
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-08-01 22:45:52 -0300

    Inicialização do desenvolvimentos das APIS.

---
+++ src/GL.API/routes/post.js
@@ -0,0 +1,12 @@
+/*
+ *
+ * Arquivo: GL.API/routes/post.js
+ * Author: Glaucia Lemos
+ * Description: Arquivo responsável pelas rotas das APIS: GET, PUT, DELETE & POST
+ * Data: 01/08/2017
+ * 
+ */
+
+//Importando o pacote 'mongoose' e o esquema criado na pasta 'models':
+var mongoose = require('mongoose');
+var Post = require('../models/post');
